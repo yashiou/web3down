9a1df8b0306388e58c5f652d0fedd89680d74aba
yashiyu <jake2292080422@gmail.com>
2024-03-04 16:03:56 +0800

    init

---
+++ Web_edu_3_down/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Web_edu_3_down.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
